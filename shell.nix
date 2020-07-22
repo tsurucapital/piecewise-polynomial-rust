0ad181e9464900fe1d9ddc1ca44ff4312f289aca
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2020-07-23 08:20:50 +0900

    Add dot for kcachegrind

--- shell.nix
+++ shell.nix
@@ -12,7 +12,7 @@ in mkShell {
   buildInputs = [
     rustChannel.rust
     # Dev tools
-    cargo-asm linuxPackages.perf valgrind kcachegrind
+    cargo-asm linuxPackages.perf valgrind kcachegrind graphviz
   ];
   # Required by test-suite and in general let's set a uniform one.
   LANG="C.UTF-8";
